f44259134a67b6f627a4865e55b839eb39ec0e3c
Luciano Garriga <lucianomgarriga@gmail.com>
2022-10-18 23:51:11 -0300

    New commit

---
+++ src/assets/data/data.js
@@ -0,0 +1,61 @@
+import img01 from "../images/img-01.jpg";
+import img02 from "../images/img-02.jpg";
+import img03 from "../images/img-03.jpg";
+import img04 from "../images/img-04.jpg";
+import img05 from "../images/img-05.jpg";
+import img06 from "../images/img-06.jpg";
+import img07 from "../images/img-07.jpg";
+import img08 from "../images/img-08.jpg";
+import img09 from "../images/img-09.jpg";
+
+export const NFT__DATA = [
+    {
+        id: "01",
+        title: "Title 1",
+        description: "Lorem ipsum lorem ipsum lorem ipsum",
+        imgUrl: img01,
+        creator: "Creator 1",
+        currentBid: 3.22,
+    },
+    {
+        id: "02",
+        title: "Title 2",
+        description: "Lorem ipsum lorem ipsum lorem ipsum",
+        imgUrl: img01,
+        creator: "Creator 2",
+        currentBid: 4.22,
+    },
+    {
+        id: "03",
+        title: "Title 3",
+        description: "Lorem ipsum lorem ipsum lorem ipsum",
+        imgUrl: img01,
+        creator: "Creator 3",
+        currentBid: 1.22,
+    },
+    {
+        id: "04",
+        title: "Title 4",
+        description: "Lorem ipsum lorem ipsum lorem ipsum",
+        imgUrl: img01,
+        creator: "Creator 4",
+        currentBid: 3.22,
+    }
+] ;
+
+export const SELLER_DATA = [
+    {
+        id: "01", 
+        description: "Lorem ipsum lorem ipsum lorem ipsum",
+        sellerName: "Seller 1",
+        // sellerImg: ava01,
+        currentBid: 3.22,
+    },
+    {
+        id: "02", 
+        description: "Lorem ipsum lorem ipsum lorem ipsum",
+        sellerName: "Seller 2",
+        // sellerImg: ava01,
+        currentBid: 3.22,
+    },
+]
