0544f3361331f6f19897c65151935e4d31e9c288
Luciano Garriga <lucianomgarriga@gmail.com>
2022-10-24 12:18:54 -0300

    Install reactstrap & remixicon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
-
 import React from 'react'
+import './header.css'
+import {Container} from 'reactstrap'
+import { NavLink, Link } from 'react-router-dom'
+
+const NAV__LINKS = [
+  {
+    display: 'Home',
+    url: '/home'
+  },
+  
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <Container>
+        <div className="navigation">
+          <div className="logo">
+            <h2 className="d-flex gap-2 align-items-center">
+              <span> <i class="ri-fire-fill"></i> </span>
+              NFTs
+            </h2>
+          </div>
+
+          <div className="nav__menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <NavLink></NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </header>
   )
 }
 
