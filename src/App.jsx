38ef389d7094efe0a7ccba3975041a19362aafd8
Luciano Garriga <lucianomgarriga@gmail.com>
2022-10-28 12:57:14 -0300

    Mod App & NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './app.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar'; 
+// import About from './components/About/about';
 
 function App() {
+  
   return(
-  <NavBar />
+    <> 
+        <NavBar/>  
+    </>
   )
 }
 
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar'; 
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './navBar.css'
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant={'dark'} expand="lg">
+    <>    
+    <Navbar bg="dark" variant={'dark'} sticky='top' expand="lg">
       <Container>
         <Navbar.Brand href="#home">NFT MARKET</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link> 
-            <Nav.Link href="#about">About Us</Nav.Link>
-            <Nav.Link href="#wallet">Connect Wallet</Nav.Link> 
+          <Nav style={{ textAlign: 'center' }}>
+            <Link to="/">Home</Link> 
+            <Link to="/about">About</Link>
+            <Link to="/wallet">Connect Wallet</Link> 
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </> 
   );
 }
  
