4f4c5ee20f065555e5acfe96e5b98d84f21dcbcc
Luciano Garriga <lucianomgarriga@gmail.com>
2022-10-29 21:48:11 -0300

    Styles in addItem button

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Title from '../Title';
+import ItemAdd from '../ItemAdd';
+
 //utilizamos destructuring, para evitar llamar los objetos por cada propiedad (props.texto)
+
 export const ItemListContainer = ({texto}) => {
   return (
     <>
         <Title greeting={texto}/>
+        <ItemAdd/>
     </>
   )
 }
