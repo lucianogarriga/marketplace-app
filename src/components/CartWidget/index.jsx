c6a9af81999e46c9499d7e78aba4c40840b27d3b
Luciano Garriga <lucianomgarriga@gmail.com>
2022-12-29 20:06:48 -0300

    Mod cartwidget

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const CartWidget = () => {
   return (
     <div style={{marginLeft: '10px'}}>
-        <i class="bi bi-bag"></i>
+        <i className="bi bi-bag"></i>
     </div>
   )
 }
