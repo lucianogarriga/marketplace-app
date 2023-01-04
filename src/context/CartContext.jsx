a7fcb90cc8ebc74e93419649d66c3c14ad85fd22
Luciano Garriga <lucianomgarriga@gmail.com>
2023-01-03 23:58:55 -0300

    Function to remove a product

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -8,15 +8,18 @@ const CartProvider = ({ children }) => {
     // LOGICA PARA EL CARRITO
     const [cart, setCart] = useState([]);
 
-    //funcion para vaciar el carrito
+    //function to clear cart
     const clearCart = () => setCart([]);
 
-    //funcion p/ saber si un producto esta en el carrito
+    //function to know if product is in cart
     const isInCart = (id) => {
         return cart.find(product => product.id === id) ? true : false;
     }
 
-
+    //function to remove a product
+    const removeProduct = (id) =>{
+        setCart(cart.filter(product => product.id !== id));
+    }
 
     return (
         <CartContext.Provider value='NFT'>
