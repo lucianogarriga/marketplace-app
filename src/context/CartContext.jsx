ffb0d81871f2860f30ab98b455d486b93c2237b5
Luciano Garriga <lucianomgarriga@gmail.com>
2023-01-03 23:56:26 -0300

    Cart functions clear & find product

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const CartContext = React.createContext([]);
 console.log('Cart Context: ', CartContext);
@@ -6,6 +6,16 @@ console.log('Cart Context: ', CartContext);
 const CartProvider = ({ children }) => {
 
     // LOGICA PARA EL CARRITO
+    const [cart, setCart] = useState([]);
+
+    //funcion para vaciar el carrito
+    const clearCart = () => setCart([]);
+
+    //funcion p/ saber si un producto esta en el carrito
+    const isInCart = (id) => {
+        return cart.find(product => product.id === id) ? true : false;
+    }
+
 
 
     return (
